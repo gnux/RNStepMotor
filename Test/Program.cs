79ddb5e5e6ab1f9cc936c311a78c5cec32920070
Georg Neubauer <georg.neubauer@student.tugraz.at>
2010-12-07 10:08:50 +0100

    First ready version, ErrorReceived is not implemented!

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using RNStepMotor;
 
 namespace Test
@@ -18,22 +15,36 @@ namespace Test
             RNCommandLibrary rn = new RNCommandLibrary();
             try
             {
-              //  rn.ResetStepCounter(MotorSelection.Motor1 | MotorSelection.Motor2);
+                //  rn.ResetStepCounter(MotorSelection.Motor1 | MotorSelection.Motor2);
                 rn.Connect("COM2");
+
+
+                rn.SetCRCMode(CRCMode.Enabled);
+                rn.ResetStepCounter(MotorSelection.All);
+                Console.WriteLine(DateTime.Now.Second + " " + DateTime.Now.Millisecond);
+                string str = rn.GetVersionAndStateInformation();
+                Console.WriteLine(str);
+                Console.WriteLine(DateTime.Now.Second + " " + DateTime.Now.Millisecond);
+                EndSwitchState[] states =  rn.GetEndSwitchStates();
+                rn.SetRotatingDirection(MotorSelection.Motor2, RotatingDirection.Right);
+                rn.SetSpeedAndAcceleration(MotorSelection.Motor2, 10, 10);
+                rn.SetCurrent(CurrentSelection.StartCurrent, MotorSelection.Motor1 | MotorSelection.Motor2, 300);
+                rn.SetCurrent(CurrentSelection.MotorCurrent, MotorSelection.Motor1 | MotorSelection.Motor2, 300);
+                rn.RotateSteps(MotorSelection.Motor2, 200);
+                //rn.RotateSteps(, 200);
+                //rn.ResetStepCounter(MotorSelection.Motor1 | MotorSelection.Motor2);
+                // rn.RotateSteps(MotorSelection.Motor1, 300);
+                // rn.RotateSteps(MotorSelection.Motor2, 300);
             }
             catch (Exception e)
             {
-            
+
             }
-            //rn.EnableCRC();
-            Console.WriteLine(DateTime.Now.Second +" "+ DateTime.Now.Millisecond);
-            string str = rn.GetVersionAndStateInformation();
-            Console.WriteLine(str);
-            Console.WriteLine(DateTime.Now.Second +" "+ DateTime.Now.Millisecond);
-            //rn.ResetStepCounter(MotorSelection.Motor1 | MotorSelection.Motor2);
-           // rn.RotateSteps(MotorSelection.Motor1, 300);
-           // rn.RotateSteps(MotorSelection.Motor2, 300);
-            rn.Disconnect();
+            finally
+            {
+                rn.Disconnect();
+                rn.Dispose();
+            }
             //byte crc = rn.TestCrc(val);
             //Console.WriteLine(crc + " 0x" + Convert.ToString(crc, 16));
             //Console.Read();
